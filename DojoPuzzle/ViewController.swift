77b06626dba208567648191fa5a6397366d75667
Makubec <bruno.guitarpro@gmail.com>
2019-02-05 21:42:55 -0200

    commit to merge

--- DojoPuzzle/ViewController.swift
+++ DojoPuzzle/ViewController.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2019 Bruno Lopes de Mello. All rights reserved.
 //
 
+/*
+ Este problema foi utilizado em 226 Dojo(s).
+ Dado um cheque com o seu valor por extenso, desenvolva um programa que retorne o seu valor numérico.
+ Por exemplo se informarmos "dois mil quinhentos e vinte e três reais e dezoito centavos", o seu programa deve retornar "2523,18"
+ */
+
+//http://dojopuzzles.com/problemas/exibe/lendo-um-cheque/#
+
 import UIKit
 
 class ViewController: UIViewController {
